85d38463f6c5b233e07fee4ec2d5ffd99f712bc7
JAY-Chan9yu <ckj9014@gmail.com>
2017-12-05 18:44:02 +0900

    map update 버그 해결, 기본적인 테트리스 기능 완성! 추가로 같은색깔로 된 길이 만들어지면 지하철이 이동하는 event 발생하는 거 추가예정

--- tetris.py
+++ tetris.py
@@ -6,14 +6,13 @@ import random
 import blocklib
 
 pygame.init()
-width, height = 1200, 700
+width, height = 1200, 1000
+mapRangeX, mapRangeY = 33, 41
 screen=pygame.display.set_mode((width, height))
 
-map = [[0] * 30 for row in range(30)] # 20 x 20  30 * 20 = 600 픽셀
+map = [[0] * mapRangeX for row in range(mapRangeY)] # 20 x 20  30 * 20 = 600 픽셀
 
-keys = [0, 0, 0, 0] # key 값 판단하는 리스트
-
-nextBlock = 0 # 다음 블럭
+keys = [0, 0, 0, 0, 0] # key 값 판단하는 리스트
 
 # colorvlue list
 BLACK = (0, 0, 0)
@@ -32,11 +31,20 @@ b3 = { 'rect':pygame.Rect(100,150,60,60), 'color':BLUE}
 blocks = [b1,b2,b3]
 
 # 블럭이 없으면 0, 있으면 1 <- 여기서 랜덤으로 2~5의 값을 주어 1이면 블럭의 색값을 부여한다.
-colors = [BLACK, RED, GREEN, BLUE, WHITE]
+colors = [BLACK, BLUE, GREEN, ORANGE, BLUE, PURPLE, BROWN]
 
-blockTimer = 100000
-blockTimer1 = 0
-blockX = 300
+missionClearEventFlag = 0 # mission clear 이벤틀 플래그
+# 게임 스피드
+blockTimer = 5000
+blockTimer1 = 5000
+changeBlockShape = 0 # 블럭의 모양을 변경하는 변수
+nowBlockShape = blocklib.block1 # 현재 나오는 블럭의 모양
+nextBlockShape = blocklib.block1 # 다음 블럭
+blockColor = ORANGE # 블럭 색상
+nextColor = BLUE # 다음 블럭 색상
+checkMoveR = 0
+checkMoveL = 0
+blockX = 300 # 현재 나오는 블럭의 x,y 좌표
 blockY = 0
 
 # load images
@@ -44,37 +52,90 @@ block = pygame.image.load("tetrisResource/image/block.jpg")
 
 def copyBlocToMap(block, X, Y) :
     global map
-    global nextBlock
+    global nextColor
+    global blockColor
+    global blockTimer1
+    global nowBlockShape
+    global nextBlockShape
 
     for i in range(0, 4) :
         for j in range(0, 4) :
-            if block[i][j] >= 1 :
-                map[int((Y / 20) + i)][int(((X - 300) / 20) + j)] = block[i][j]
-    #map[28][28] = 1#int(block[i][j])
+            # 지하철 노선에 따른 색깔
+            tempColor = 0
+            if blockColor == BLUE:
+                tempColor = 1
+            elif blockColor == GREEN:
+                tempColor = 2
+            elif blockColor == ORANGE :
+                tempColor = 3
+            elif blockColor == BLUE :
+                tempColor = 4
+            elif blockColor == PURPLE :
+                tempColor = 5
+            elif blockColor == BROWN :
+                tempColor = 6
 
-    nextBlock = random.randint(0, 3)
+            if block[(changeBlockShape * 4) + i][j] >= 1 :
+                map[int((Y / 20) + i)][int(((X - 300) / 20) + j)] = tempColor
+
+    blockTimer1 = 5000
+    blockColor = nextColor
+    nowBlockShape = nextBlockShape
+    nextBlockShape = blocklib.randBlock(random.randint(0, 3))
+    nextColor = colors[random.randint(1, 6)]
+
+def missionClearEvent() :
 
 
 if __name__ == "__main__":
+    nextBlockShape = blocklib.randBlock(random.randint(0, 3))
+    # 오른쪽 벽 채우기(오른쪽으로 더이상 이동 못하게)
+    for i in range(0, mapRangeY) :
+        map[i][30] = 88
+    for i in range(1, 31) :
+        map[39][i] = 1
+
     while 1:
         blockTimer -= 1
         #screen.fill(0)
 
         # block update
         if blockTimer == 0 :
-            blockTimer = 100 - (blockTimer1 * 2)
+            blockTimer = blockTimer1
             screen.fill(0)
 
-            #update map
-            for i in blocklib.block1[3] :
-                if (i == 1 and map[int(blockY / 20)+1][int((blockX - 300) / 20)] == 1) \
-                    or (blockY + 60 == 580) :
-                    #tempa =1
-                    copyBlocToMap(blocklib.block1, blockX, blockY)
-                    blockX = 300
-                    blockY = 0
+            # update map
+            checkMoveL = checkMoveR = 1
+            for i in range(3, 0, -1) :
+                yBlock = nowBlockShape[(changeBlockShape * 4) + i]
+                # map에 블럭이 내려올때 바로 아래단에 블럭이 차있는지 확인 or 바닥에 닿았을 경우
+                for j, tBlock in enumerate(yBlock) :
+                    if ((tBlock == 1 and map[int(blockY / 20) + i + 1][int((blockX - 300) / 20) + j] >= 1)
+                            or (blockY + 60 == ((mapRangeY - 1) * 20) - 20)) :
+                        copyBlocToMap(nowBlockShape, blockX, blockY)
+                        blockX = 300
+                        blockY = 0
+                    if (tBlock == 1 and map[int(blockY / 20) + i][int((blockX - 300) / 20) + j - 1] >= 1) :
+                        checkMoveL = 0
+                    if (tBlock == 1 and map[int(blockY / 20) + i][int((blockX - 300) / 20) + j + 1] >= 1) :
+                        checkMoveR = 0
 
-            # draw map
+            # checkFillBlock
+            for i in range(mapRangeY - 2, 0, -1) :
+                yMap = map[i]
+                checkLine = 0
+                for xMap in yMap : # 라인 체크
+                    if xMap >= 1 :
+                        checkLine += 1
+                if checkLine >= 31 : # 라인이 한줄 꽉찰경우
+                    for t in range(mapRangeY - 2, 1, -1) :
+                        for k in range(0, 31) :
+                            map[t][k] = map[t - 1][k]
+                    for k in range(0, 31) :
+                        map[0][k] = 0
+                checkLine = 0
+
+            # draw map.
             for i, yMap in enumerate(map) :
                 tempX = 300
                 tempY = 20 * i
@@ -82,7 +143,9 @@ if __name__ == "__main__":
                     tempColor = 0
 
                     # 지하철 노선에 따른 색깔
-                    if xMap == 2:
+                    if xMap == 1:
+                        tempColor = BLUE
+                    elif xMap == 2:
                         tempColor = GREEN
                     elif xMap == 3 :
                         tempColor = ORANGE
@@ -95,29 +158,29 @@ if __name__ == "__main__":
 
                     if xMap >= 1 :
                         #pygame.draw.rect(screen, colors[random.randint(0,4)], pygame.Rect(tempX, tempY, 20, 20))
-                        pygame.draw.rect(screen, ORANGE, pygame.Rect(tempX, tempY, 20, 20))
-                        tempX += 20
+                        pygame.draw.rect(screen, tempColor, pygame.Rect(tempX, tempY, 20, 20))
+                    tempX += 20
 
             # draw now block
-            for i, yBlock in enumerate(blocklib.block1) :
+            for i in range(0, 4) :
+                tBlock = nowBlockShape
+                yBlock = tBlock[(changeBlockShape * 4) + i]
                 tempX = blockX
                 tempY = blockY + (20 * i)
                 for j, xBlock  in enumerate(yBlock) :
-                    tempColor = 0
-                    if xBlock == 1 :
-                        tempColor = ORANGE
-                    pygame.draw.rect(screen, tempColor, pygame.Rect(tempX, tempY, 20, 20))
+                    if xBlock >= 1 :
+                        pygame.draw.rect(screen, blockColor, pygame.Rect(tempX, tempY, 20, 20))
                     tempX += 20
 
             # draw next block
-            for i, yBlock in enumerate(blocklib.randBlock(nextBlock)) :
+            for i in range(0, 4) :
+                tBlock = nextBlockShape
+                yBlock = tBlock[(changeBlockShape * 4) + i]
                 tempX = 940
                 tempY = 20 * i
-                for j, xBlock  in enumerate(yBlock) :
-                    tempColor = 0
-                    if xBlock == 1 :
-                        tempColor = ORANGE
-                    pygame.draw.rect(screen, tempColor, pygame.Rect(tempX, tempY, 20, 20))
+                for j, xBlock in enumerate(yBlock) :
+                    if xBlock >= 1 :
+                        pygame.draw.rect(screen, nextColor, pygame.Rect(tempX, tempY, 20, 20))
                     tempX += 20
 
             # draw background
@@ -127,42 +190,46 @@ if __name__ == "__main__":
                 if i == 1 :
                     tempW = 900
                     tempN = 0
-                for j in range(0, 31) :
+                for j in range(0, mapRangeY) :
                     screen.blit(block, (tempW, tempN))
                     tempN += 20
             tempN = 280
-            for i in range(0, 32) :
-                screen.blit(block, (tempN, 600))
+            for i in range(0, mapRangeX - 2) :
+                screen.blit(block, (tempN, (mapRangeY - 1) * 20))
                 tempN += 20
 
             # update screen
             pygame.display.flip()
 
-            if blockY < 520  :
+            if blockY < ((mapRangeY - 1) * 20) - 80 :
                 blockY += 1 # 아래로 내려가기
 
         # loop through the events
         for event in pygame.event.get():
             # check if the event is the X button
             if event.type==pygame.KEYDOWN: # key가 눌렸을때
-                if event.key==K_UP:
-                    keys[0]=1
-                elif event.key==K_DOWN:
-                    keys[1]=1
-                elif event.key==K_LEFT and blockX > 300 :
-                    keys[2]=1
-                elif event.key==K_RIGHT and blockX < 900:
-                    keys[3]=1
+                if event.key == K_UP:
+                    keys[0] = 1
+                elif event.key == K_DOWN:
+                    keys[1] = 1
+                elif event.key == K_LEFT and blockX > 300 and checkMoveL == 1:
+                    keys[2] = 1
+                elif event.key == K_RIGHT and blockX < 900 and checkMoveR == 1:
+                    keys[3] = 1
+                elif event.key == K_SPACE :
+                    keys[4] = 1
 
             if event.type==pygame.KEYUP: # key가 떨어졌을때
-                if keys[0] == 1 and event.key==pygame.K_UP:
-                    keys[0]=2
-                elif keys[1] == 1 and event.key==pygame.K_DOWN:
-                    keys[1]=2
-                elif keys[2] == 1 and event.key==pygame.K_LEFT:
-                    keys[2]=2
-                elif keys[3] == 1 and event.key==pygame.K_RIGHT:
-                    keys[3]=2
+                if keys[0] == 1 and event.key == K_UP:
+                    keys[0] = 2
+                elif keys[1] == 1 and event.key == K_DOWN:
+                    keys[1] = 2
+                elif keys[2] == 1 and event.key == K_LEFT:
+                    keys[2] = 2
+                elif keys[3] == 1 and event.key == K_RIGHT:
+                    keys[3] = 2
+                elif keys[4] == 1 and event.key == K_SPACE :
+                    keys[4] = 2
 
             if event.type==pygame.QUIT:
                 # if it is quit the game
@@ -171,8 +238,19 @@ if __name__ == "__main__":
 
         # Move playe
         if keys[2] == 2 :
-            blockX-=20
+            blockX -= 20
             keys[2] = 0
         elif keys[3] == 2 :
-            blockX+=20
+            blockX += 20
             keys[3] = 0
+        elif keys[0] == 2 :
+            changeBlockShape += 1
+            if changeBlockShape >= 4 :
+                changeBlockShape = 0
+            keys[0] = 0
+        elif keys[1] == 2 :
+            blockTimer1 = 5
+            keys[1] = 0
+        elif keys[4] == 2 :
+            missionClearEventFlag = 1
+            keys[4] = 0
