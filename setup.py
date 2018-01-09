5c116cf34447683852e5c2a3488b17e6951ae550
JAY-Chan9yu <ckj9014@gmail.com>
2018-01-09 23:55:57 +0900

    [메트리스 exe파일, 설치파일 ver 1.0 배포]

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 import sys
 from cx_Freeze import setup, Executable
-
+import glob
 base = None
 
-build_exe_options = dict(
-        compressed = True,
-        includes = ["time","tetris","blocklib","random","math", "pygame"],
-        include_files = []
-)
 packages = ["time","tetris","blocklib","random","math", "pygame"]
 options = {
     'build_exe': {
@@ -15,10 +10,19 @@ options = {
     },
 }
 
+'''data_files = []
+directories = glob.glob('E:/GitSource/pygame/tetrisResource/image/')
+for directory in directories:
+    files = glob.glob(directory+'*')
+    data_files.append((directory, files))'''
+
 setup(
     name="Metris",
     version="1.0",
     options = options,
-    description = "실행 파일로 배포",
-    author="JAYJI",
-    executables = [Executable("playgame.py", base="Win32GUI")])
+    #data_files = data_files,
+    #["E:/GitSource/pygame/tetrisResource/image/*", "E:/GitSource/pygame/tetrisResource/audio/*", "E:/GitSource/pygame/tetrisResource/font/*"],
+    description = "메트리스(메트로+테트리스) 게임 입니다.",
+    author="JayJi(지찬규)",
+    author_email = "ckj9014@gmail.com",
+    executables = [Executable("playgame.py", base="Win32GUI", icon="icon.ico")])
