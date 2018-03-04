d1450a21e7b86551ff3e7897b2ab931575ee0af8
JAY-Chan9yu <ckj9014@gmail.com>
2018-03-04 21:50:23 +0900

    [setup 불필요한 내용 삭제]

--- setup.py
+++ setup.py
@@ -10,19 +10,10 @@ options = {
     },
 }
 
-'''data_files = []
-directories = glob.glob('E:/GitSource/pygame/tetrisResource/image/')
-for directory in directories:
-    files = glob.glob(directory+'*')
-    data_files.append((directory, files))'''
-
 setup(
     name="Metris",
     version="1.0",
     options = options,
-    #data_files = data_files,
-    #["E:/GitSource/pygame/tetrisResource/image/*", "E:/GitSource/pygame/tetrisResource/audio/*", "E:/GitSource/pygame/tetrisResource/font/*"],
-    description = "메트리스(메트로+테트리스) 게임 입니다.",
     author="JayJi(지찬규)",
     author_email = "ckj9014@gmail.com",
     executables = [Executable("playgame.py", base="Win32GUI", icon="icon.ico")])
